d44e64dfb01de361af7ea96cdc695b1adfc3a9f6
Omar Saadoun <osaadoun@inmindsoftware.com>
2024-06-18 18:21:09 -0300

    fix caps 2

--- server.py
+++ server.py
@@ -37,7 +37,7 @@ def generate_response():
             # Load the model and tokenizer if not previously loaded
             if model is None:
                 # Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
-                model = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7B-Chat-GGUF", model_file="llama-2-7b-chat.q4_K_M.gguf", model_type="llama", gpu_layers=50)
+                model = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7B-Chat-GGUF", model_file="llama-2-7b-chat.Q4_K_M.gguf", model_type="llama", gpu_layers=50)
               #  tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
               #  model = ORTModelForSeq2SeqLM.from_pretrained(model_path, from_transformers=True)
               #  model.to("cuda")
