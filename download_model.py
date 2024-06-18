9c958e63818b33edfe7293b1d916c612278917d4
Omar Saadoun <osaadoun@inmindsoftware.com>
2024-06-18 18:17:57 -0300

    fix to snapshot download

--- download_model.py
+++ download_model.py
@@ -1,11 +1,11 @@
 import sys
-from huggingface_hub import hf_hub_download
+from huggingface_hub import snapshot_download
 
-model_url = sys.argv[1]
-model_path = sys.argv[2]
+model_url = sys.argv[0] 
 
-print(f"Starting download of {model_url} and {model_path}")
+print(f"Starting download of {model_url}")
 
-hf_hub_download(repo_id=model_url,filename=model_path, repo_type="model", local_dir="/app/repo")
+#hf_hub_download(repo_id=model_url,filename=model_path, repo_type="model")
+snapshot_download(repo_id=model_url)
 
-print(f"Model downloaded to {model_path}")
+print(f"Model downloaded to {model_url}")
