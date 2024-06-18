373408457b3ae16bc0b5a8fc80f9bcaf4d3f6448
Omar Saadoun <osaadoun@inmindsoftware.com>
2024-06-18 17:15:10 -0300

    fix

--- download_model.py
+++ download_model.py
@@ -6,6 +6,6 @@ model_path = sys.argv[2]
 
 print(f"Starting download of {model_url} and {model_path}")
 
-hf_hub_download(repo_id=model_url,filename=model_path, repo_type="model", local_dir="/app/repo">
+hf_hub_download(repo_id=model_url,filename=model_path, repo_type="model", local_dir="/app/repo")
 
 print(f"Model downloaded to {model_path}")
