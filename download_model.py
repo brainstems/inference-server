7037dca2cbdb29306c1eba2e3c52456b55210d58
Ernesto Messina <ernestom@brainstems.ai>
2024-07-04 19:43:44 -0300

    model file path.

--- download_model.py
+++ download_model.py
@@ -6,6 +6,7 @@ model_path = sys.argv[2]
 
 print(f"Starting download of {model_url}")
 
-hf_hub_download(repo_id=model_url, filename=model_path, repo_type="model")
+#hf_hub_download(repo_id=model_url, filename=model_path, repo_type="model")
+hf_hub_download(repo_id=model_url, filename=model_path, local_dir="/app/repo/brainstems-jedai-akash-poc/", repo_type="model")
 
 print(f"Model downloaded to {model_url}")
