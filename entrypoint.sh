d59f88d871ea2d7d7139e7bc311bad873cae2121
Ernesto Messina <ernestom@brainstems.ai>
2024-07-04 10:51:36 -0300

    Fix repo path.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,15 @@
 # Variables
 REPO_URL="https://github.com/brainstems/brainstems-jedai-akash-poc.git"
 REPO_BRANCH=test/newer-cuda-image
-REPO_DIR="/app/repo"
+REPO_DIR="/app/repo/brainstems-jedai-akash-poc"
 MODEL_URL=${MODEL_URL:-"TheBloke/Llama-2-7B-Chat-GGUF"}
 MODEL_PATH=${MODEL_PATH:-"llama-2-7b-chat.Q5_K_S.gguf"}
 
 # Clone the repository
 echo "Cloning repository branch $REPO_BRANCH"
-git clone -b "$REPO_BRANCH" "$REPO_URL" "$REPO_DIR"
+git clone -b "$REPO_BRANCH" "$REPO_URL"
 
-Download the model file
+# Download the model file
 if [ ! -f "$MODEL_PATH" ]; then
     echo "Downloading the model file..."
     python3 "$REPO_DIR"/download_model.py "$MODEL_URL" "$MODEL_PATH"
