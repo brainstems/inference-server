513f6c5583ec1a1b12c0de608946634ccfb1790f
Ernesto Messina <ernestom@brainstems.ai>
2024-07-04 22:33:22 -0300

    deactivating download script

--- download_model.py
+++ download_model.py
@@ -4,10 +4,9 @@ from huggingface_hub import hf_hub_download
 model_url = sys.argv[1]
 model_path = sys.argv[2]
 
-print(f"Starting download of {model_url}")
+#print(f"Starting download of {model_url}")
 
 #hf_hub_download(repo_id=model_url, filename=model_path, repo_type="model")
-hf_hub_download(repo_id=model_url, filename=model_path, local_dir="/app/repo/brainstems-jedai-akash-poc/", repo_type="model")
-#hf_hub_download(repo_id=model_url, filename=model_path, local_dir=".", repo_type="model")
+#hf_hub_download(repo_id=model_url, filename=model_path, local_dir="/app/repo/brainstems-jedai-akash-poc/", repo_type="model")
 
-print(f"Model downloaded to {model_url}")
+#print(f"Model downloaded to {model_url}")
