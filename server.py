6a309158507aa0a39aeca98a7d1aad632c878165
Ernesto Messina <ernestom@brainstems.ai>
2024-07-08 14:44:45 -0300

    Using model from cognitivecomputations

--- server.py
+++ server.py
@@ -9,10 +9,11 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"device set to: {device}")
 
 # Load tokenizer
-tokenizer = AutoTokenizer.from_pretrained("TheBloke/dolphin-2.0-mistral-7B-GGUF")
+tokenizer = AutoTokenizer.from_pretrained("cognitivecomputations/dolphin-2.0-mistral-7b")
 
 # Load model
-model = AutoModelForCausalLM.from_pretrained("TheBloke/dolphin-2.0-mistral-7B-GGUF", model_file="dolphin-2.0-mistral-7b.Q4_K_M.gguf", model_type="mistral", gpu_layers=50)
+#model = AutoModelForCausalLM.from_pretrained("TheBloke/dolphin-2.0-mistral-7B-GGUF", model_file="dolphin-2.0-mistral-7b.Q4_K_M.gguf", model_type="mistral", gpu_layers=50, token=access_token)
+model = AutoModelForCausalLM.from_pretrained("cognitivecomputations/dolphin-2.0-mistral-7b")
 
 @app.route('/generate', methods=['POST'])
 def generate():
