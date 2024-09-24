47431c2f8306f2cbaa2ad97a517fa1c8772c9c9b
Kevyn Franco <kevyn@franco.expert>
2024-09-23 22:45:53 -0500

    feat/model-manager-update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
 # Variables
-REPO_LOCAL_PATH="/app/repo/inference-server"
+REPO_LOCAL_PATH="/app/repo"
 
-# Change to the app directory
-cd "/app/repo"
-
-# Clone the repository
-echo "Cloning repository branch $REPO_BRANCH"
-git clone -b "$REPO_BRANCH" "$REPO_URL"
-
-# Move the model dir to the inference-server dir.
-echo "Moving files to repo dir"
-mv model/ inference-server/
-
+# Cambia al directorio de la aplicación
 cd "$REPO_LOCAL_PATH"
 
-# Run the application
+# Ejecuta el servidor
 echo "Running server"
 python3 src/server.py
